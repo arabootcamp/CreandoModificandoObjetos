ddc1af66b565fb2aa3f97fa8ff220065d685ed0e
aracelly <aracellybootcamp@gmail.com>
2021-07-26 22:23:53 -0400

    base del proyecto

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/*1. Crear todo el código usando ES5.*/
+/*2. Crear una función constructora para cada objeto.*/
+/*3. Implementar métodos getters y setters para poder acceder y modificar los datos de
+los pacientes.*/
+/*4. Crear un método mediante la propiedad prototype que permita buscar los datos de
+los usuarios por nombre y otro método que permita mostrar todos los datos de los
+usuarios registrados.*/
+/*5. Instanciar cada objeto utilizando la instrucción new.*/
